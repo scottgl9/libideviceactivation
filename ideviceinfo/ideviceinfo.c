22299c837820394991a035e343fb154c08887c0a
¨Scott <scott.glover@r1soft.com>
2016-11-02 11:23:32 -0500

    Added ability to parse ActivationInfoXML.

--- ideviceinfo/ideviceinfo.c
+++ ideviceinfo/ideviceinfo.c
@@ -48,7 +48,7 @@ static const char *domains[] = {
 	"com.apple.mobile.lockdownd",
 	"com.apple.mobile.lockdown_cache",
 	//"com.apple.xcode.developerdomain",
-	"com.apple.international",
+	//"com.apple.international",
 	"com.apple.mobile.data_sync",
 	"com.apple.mobile.backup",
 	//"com.apple.mobile.nikita",
@@ -63,6 +63,88 @@ static const char *domains[] = {
 	"com.apple.mobile.iTunes",
 	NULL
 };
+
+/* aaaack but it's fast and const should make it shared text page. */
+static const unsigned char pr2six[256] =
+{
+    /* ASCII table */
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
+    64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
+    64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
+};
+
+int Base64decode_len(const char *bufcoded)
+{
+    int nbytesdecoded;
+    register const unsigned char *bufin;
+    register int nprbytes;
+
+    bufin = (const unsigned char *) bufcoded;
+    while (pr2six[*(bufin++)] <= 63);
+
+    nprbytes = (bufin - (const unsigned char *) bufcoded) - 1;
+    nbytesdecoded = ((nprbytes + 3) / 4) * 3;
+
+    return nbytesdecoded + 1;
+}
+
+int Base64decode(char *bufplain, const char *bufcoded)
+{
+    int nbytesdecoded;
+    register const unsigned char *bufin;
+    register unsigned char *bufout;
+    register int nprbytes;
+
+    bufin = (const unsigned char *) bufcoded;
+    while (pr2six[*(bufin++)] <= 63);
+    nprbytes = (bufin - (const unsigned char *) bufcoded) - 1;
+    nbytesdecoded = ((nprbytes + 3) / 4) * 3;
+
+    bufout = (unsigned char *) bufplain;
+    bufin = (const unsigned char *) bufcoded;
+
+    while (nprbytes > 4) {
+    *(bufout++) =
+        (unsigned char) (pr2six[*bufin] << 2 | pr2six[bufin[1]] >> 4);
+    *(bufout++) =
+        (unsigned char) (pr2six[bufin[1]] << 4 | pr2six[bufin[2]] >> 2);
+    *(bufout++) =
+        (unsigned char) (pr2six[bufin[2]] << 6 | pr2six[bufin[3]]);
+    bufin += 4;
+    nprbytes -= 4;
+    }
+
+    /* Note: (nprbytes == 1) would be an error, so just ingore that case */
+    if (nprbytes > 1) {
+    *(bufout++) =
+        (unsigned char) (pr2six[*bufin] << 2 | pr2six[bufin[1]] >> 4);
+    }
+    if (nprbytes > 2) {
+    *(bufout++) =
+        (unsigned char) (pr2six[bufin[1]] << 4 | pr2six[bufin[2]] >> 2);
+    }
+    if (nprbytes > 3) {
+    *(bufout++) =
+        (unsigned char) (pr2six[bufin[2]] << 6 | pr2six[bufin[3]]);
+    }
+
+    *(bufout++) = '\0';
+    nbytesdecoded -= (4 - nprbytes) & 3;
+    return nbytesdecoded;
+}
 
 static int is_domain_known(char *domain)
 {
@@ -99,6 +181,12 @@ static void print_usage(int argc, char **argv)
 	//printf("Homepage: <" PACKAGE_URL ">\n");
 }
 
+void write_xml_file(char *filename, char *xml, uint32_t len) {
+	FILE *f = fopen(filename, "wb");
+	fwrite(xml, 1, len, f);
+	fclose(f);
+}
+
 int main(int argc, char *argv[])
 {
 	lockdownd_client_t client = NULL;
@@ -112,7 +200,9 @@ int main(int argc, char *argv[])
 	char *domain = NULL;
 	char *key = NULL;
 	char *xml_doc = NULL;
+	char *str = NULL;
 	uint32_t xml_length;
+	uint64_t len;
 	plist_t node = NULL;
 
 	/* parse cmdline args */
@@ -187,21 +277,10 @@ int main(int argc, char *argv[])
 	if(lockdownd_get_value(client, domain, key, &node) == LOCKDOWN_E_SUCCESS) {
 		
 		if (node) {
-			switch (format) {
-			case FORMAT_XML:
-				plist_to_xml(node, &xml_doc, &xml_length);
-				printf("%s", xml_doc);
-				free(xml_doc);
-				xml_doc = NULL;
-				break;
-			case FORMAT_KEY_VALUE:
-				plist_print_to_stream(node, stdout);
-				break;
-			default:
-				if (key != NULL)
-					plist_print_to_stream(node, stdout);
-			break;
-			}
+			plist_to_xml(node, &xml_doc, &xml_length);
+			printf("%s", xml_doc);
+			free(xml_doc);
+			xml_doc = NULL;
 			plist_free(node);
 			node = NULL;
 		}
@@ -211,34 +290,50 @@ int main(int argc, char *argv[])
 		if (domains[i] == NULL) break;
 		if(lockdownd_get_value(client, domains[i], key, &node) == LOCKDOWN_E_SUCCESS && node) {
 			printf("Domain %s:\n", domains[i]);
-			switch (format) {
-			case FORMAT_XML:
-                                plist_to_xml(node, &xml_doc, &xml_length);
-                                printf("%s", xml_doc);
-                                free(xml_doc);
-				xml_doc = NULL;
-                                break;
-                        case FORMAT_KEY_VALUE:
-                                plist_print_to_stream(node, stdout);
-                                break;
-                        default:
-                                if (key != NULL)
-                                        plist_print_to_stream(node, stdout);
-                        	break;
-
-			}
+			plist_to_xml(node, &xml_doc, &xml_length);
+			printf("%s", xml_doc);
+			free(xml_doc);
+			xml_doc = NULL;
 			plist_free(node);
 			node = NULL;
 		}
 	}
+
+	if(lockdownd_get_value(client, "com.apple.mobile.iTunes", key, &node) == LOCKDOWN_E_SUCCESS && node) {
+		// important fields: FairPlayCertificate, FairPlayGUID, FairPlayID, MinITunesVersion
+		char *tmp_value=NULL;
+		plist_t item = plist_dict_get_item(node, "FairPlayCertificate");
+		if (!item) printf("Failed to get FairPlayCertificate\n");
+		plist_to_xml(item, &xml_doc, &xml_length);
+		write_xml_file("FairPlayCertificate.crt", xml_doc, xml_length);
+		//plist_free(item);
+		item = plist_dict_get_item(node, "FairPlayGUID");
+		//plist_get_string_val(item, &tmp_value);	
+		//printf("FairPlayGUID = %s\n", tmp_value);
+		plist_free(node);
+		node = NULL;
+	}
+
 	if ((lockdownd_get_value(client, NULL, "ActivationInfo", &node) != LOCKDOWN_E_SUCCESS) || !node || (plist_get_node_type(node) != PLIST_DICT)) {
 		fprintf(stderr, "%s: Unable to get ActivationInfo from lockdownd\n", __func__);
 	} else {
 		char *tmp_value=NULL;
+		plist_t subitem;
 		plist_t item = plist_dict_get_item(node, "ActivationInfoXML");
-		plist_get_string_val(item, &tmp_value);
-		//plist_to_xml(item, &xml_doc, &xml_length);
-		printf("%s\n",tmp_value);
+		if (!item) printf("Failed to get ActivationInfoXML\n");
+
+		plist_get_data_val(item, &xml_doc, &len);
+		item = NULL;
+
+		// load ActivationInfoXML as plist
+		plist_from_xml(xml_doc, len, &item);
+		write_xml_file("ActivationInfoXML.xml", xml_doc, (uint32_t)len);
+
+		// get DeviceCertRequest
+		subitem = plist_dict_get_item(item, "DeviceCertRequest");
+                if (!subitem) printf("Failed to get DeviceCertRequest\n");
+		plist_get_data_val(subitem, &xml_doc, &len);
+		write_xml_file("DeviceCertRequest.cer", xml_doc, (uint32_t)len);
 		plist_free(node);
 	}
 	if (domain != NULL)
